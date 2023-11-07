48d2c030f067258ad6b8657482b13c891f60bfb3
Oscar <omatmar964@g.educaand.es>
2023-11-07 23:55:07 +0100

    imagenes tipos

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs/internal/Observable';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +16,14 @@ export class PokemonService {
     const url = `${this.pokeapi}?limit=493`;
     return this.http.get(url);
   }
+
+  getPokemonTypes(pokemonNameOrId: string): Observable<string[]> {
+    const url = `${this.pokeapi}${pokemonNameOrId}`;
+    return this.http.get(url).pipe(
+      map((pokemonData: any) => {
+        return pokemonData.types.map((typeObj: any) => typeObj.type.name);
+      })
+    );
+}
 }
 
--- src/app/pokemonlist/pokemonlist.component.ts
+++ src/app/pokemonlist/pokemonlist.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../pokemon.service';
+import { ObservableInput, forkJoin } from 'rxjs';
+
 
 @Component({
   selector: 'app-pokemonlist',
@@ -12,9 +14,21 @@ export class PokemonlistComponent implements OnInit {
   constructor(private pokemonService: PokemonService) {}
 
   ngOnInit() {
-    this.pokemonService.getPokemonList().subscribe((data: any) => {
-      console.log(data);
+    const getPokemonList$ = this.pokemonService.getPokemonList();
+    const pokemonObservables: ObservableInput<any> = [];
+
+    getPokemonList$.subscribe((data: any) => {
       this.pokemonList = data.results;
+
+      const pokemonObservables = this.pokemonList.map((pokemon: any) => {
+        return this.pokemonService.getPokemonTypes(pokemon.name);
+      });
+      
+      forkJoin(pokemonObservables).subscribe((pokemonTypes: string[][]) => {
+        this.pokemonList.forEach((pokemon, index) => {
+          pokemon.types = pokemonTypes[index];
+        });
+      });
     });
   }
 
@@ -22,6 +36,7 @@ export class PokemonlistComponent implements OnInit {
     const pokemonId = pokemonUrl.split('/').filter(segment => !!segment).pop();
     return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png`;
   }
+  
   
   
 }
