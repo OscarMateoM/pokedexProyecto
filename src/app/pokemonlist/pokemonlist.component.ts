7dfe50b563aa0b4f93edf7aaf5695a308092d17d
Oscar <omatmar964@g.educaand.es>
2023-11-08 12:47:08 +0100

    numeros de tres cifras

--- src/app/pokemonlist/pokemonlist.component.ts
+++ src/app/pokemonlist/pokemonlist.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../pokemon.service';
-import { ObservableInput, forkJoin } from 'rxjs';
+import { forkJoin } from 'rxjs';
 
 
 @Component({
